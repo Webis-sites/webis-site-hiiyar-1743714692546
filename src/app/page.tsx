177d1c9dd4242efc529ad9af2d8a316af9313748
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-04 00:16:41 +0300

    Add TestimonialsCarousel component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
 import PortfolioGallery from '../components/PortfolioGallery';
 import BookingSystem from '../components/BookingSystem';
+import TestimonialsCarousel from '../components/TestimonialsCarousel';
 
 export default function HomePage() {
   return (
@@ -23,6 +24,7 @@ export default function HomePage() {
     <ServicesSection />
     <PortfolioGallery />
     <BookingSystem />
+    <TestimonialsCarousel />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
